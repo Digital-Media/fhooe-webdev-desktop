7ae1faa4d0d964ecd21ac97b6cd6d515e52bc4fe
Martin Harrer <18611980+martinharrer@users.noreply.github.com>
2020-05-20 14:14:57 +0200

    Update install-ElasticSearch.sh

--- install-ElasticSearch.sh
+++ install-ElasticSearch.sh
@@ -9,15 +9,15 @@ wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add
 sudo bash -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
 sudo apt-get update
 sudo apt-get install elasticsearch
-# sudo vi /etc/elasticsearch/elasticsearch.yml
 
-# configuring elasticsearch only for localhost.
+# configuring elasticsearch for localhost only.
+# https://www.elastic.co/guide/en/elasticsearch/reference/current/important-settings.html
 # https://www.elastic.co/guide/en/elasticsearch/reference/7.7/breaking-changes-7.7.html#breaking_77_discovery_changes
+# sudo vi /etc/elasticsearch/elasticsearch.yml
 # network.host: localhost
 # cluster.name: myShopCluster1
 # node.name: "myShopNode1"
 
-# https://www.elastic.co/guide/en/elasticsearch/reference/current/important-settings.html
 
 sudo /bin/systemctl enable elasticsearch.service
 echo "## starting elasticsearch - that can take a while ##"
