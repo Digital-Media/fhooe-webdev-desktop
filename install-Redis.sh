2e89626ceab8a53002173fed29ff860c60aff0a8
Martin Harrer <18611980+martinharrer@users.noreply.github.com>
2020-06-03 14:27:21 +0200

    Update install-Redis.sh

--- install-Redis.sh
+++ install-Redis.sh
@@ -9,6 +9,7 @@ make
 make test
 sudo bash -c "echo 'cd $HOME/Downloads/redis-stable' > /usr/local/bin/StartRedis.sh"
 sudo bash -c "echo 'nohup ./src/redis-server ./redis.conf &>redis.out &' >> /usr/local/bin/StartRedis.sh"
+sudo bash -c "echo 'sudo netstat -nlp|grep redis' >> /usr/local/bin/StartRedis.sh"
 sudo bash -c "echo 'cd $HOME/Downloads/redis-stable'  > /usr/local/bin/StopRedis.sh"
 var='pid=`ps -ef|grep redis|head -1|awk '
 var+="'"
@@ -19,10 +20,12 @@ sudo chmod o+w /usr/local/bin/StopRedis.sh
 echo $var >> /usr/local/bin/StopRedis.sh
 sudo chmod o-w /usr/local/bin/StopRedis.sh
 sudo bash -c "echo 'kill -15 \$pid' >> /usr/local/bin/StopRedis.sh"
+sudo bash -c "echo 'sleep 10' >> /usr/local/bin/StopRedis.sh"
+sudo bash -c "echo 'sudo netstat -nlp|grep redis' >> /usr/local/bin/StopRedis.sh"
 sudo bash -c "echo 'cd $HOME/Downloads/redis-stable'  >> /usr/local/bin/StopRedis.sh"
 sudo bash -c "echo 'rm redis.out dump.rdb' >> /usr/local/bin/StopRedis.sh"
 sudo chmod a+x /usr/local/bin/*Redis.sh
 cd $HOME/Downloads
 rm redis-stable.tar.gz
-cd redis-stable
-sed -i 's/# bind 127.0.0.1 ::1/bind 127.0.0.1 ::1/g' redis.conf
+sed -i 's/# requirepass foobared/requirepass geheim/g' $HOME/Downloads/redis-stable/redis.conf
+sudo apt-get install php-redis
