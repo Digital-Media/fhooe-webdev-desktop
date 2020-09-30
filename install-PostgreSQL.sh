3649c48bed3f58dadfc58ec91cd0b3878a2155f6
Martin Harrer <18611980+martinharrer@users.noreply.github.com>
2020-09-30 12:47:22 +0200

    Update install-PostgreSQL.sh

--- install-PostgreSQL.sh
+++ install-PostgreSQL.sh
@@ -1,18 +1,20 @@
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get -y install postgresql
+sudo apt-get -y -qq install postgresql
+
+
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt-get -y -qq install pgadmin4-desktop
+
+sudo su - postgres -c "psql -c \"alter user postgres with password 'geheim'\""
+sudo su - postgres -c "psql -c \"alter user postgres with password 'geheim'\""
+sudo su - postgres -c "psql -c \"alter user postgres with password 'geheim'\""
+sudo su - postgres -c "psql -c \"CREATE DATABASE onlineshop\""
+sudo su - postgres -c "psql -c \"CREATE USER onlineshop WITH ENCRYPTED PASSWORD 'geheim'\""
+sudo su - postgres -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE onlineshop to onlineshop\""
+
 # systemctl status postgresql.service
 # systemctl status postgresql@12-main.service
 # systemctl is-enabled postgresql
-sudo su - postgres
-psql -c "alter user postgres with password 'geheim'"
-psql -c "alter user postgres with password 'geheim'"
-psql -c "CREATE DATABASE onlineshop"
-psql -c "CREATE USER onlineshop WITH ENCRYPTED PASSWORD 'geheim'"
-psql -c "GRANT ALL PRIVILEGES ON DATABASE onlineshop to onlineshop"
-
-exit
-sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
-sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
-sudo apt-get -y install pgadmin4-desktop
