aeea1b57f419322fd878b02dce6a250bb843e22d
Martin Harrer <18611980+martinharrer@users.noreply.github.com>
2021-04-22 12:28:32 +0200

    Update fhooe-webdev-installation.sh
    moved some tools to basic_tools.sh

--- fhooe-webdev-installation.sh
+++ fhooe-webdev-installation.sh
@@ -13,15 +13,6 @@ echo "#############################################"
 sudo apt-get -y install apache2 php7.4 libapache2-mod-php7.4
 sudo apt-get -y install php7.4-zip php7.4-mysql php7.4-curl php7.4-dev php7.4-gd php7.4-intl php-pear php-imagick php7.4-imap php7.4-tidy php7.4-xmlrpc php7.4-xsl php7.4-mbstring php7.4-xml php-xdebug
 
-echo "## Installing zip, unzip, curl, net-tools ##"
-
-sudo apt-get -y install zip unzip curl net-tools
-echo "## Installing chrome -- only chromium can be installed with apt-get ##"
-
-cd /home/ubuntu/Downloads
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-
 echo "###############################################"
 echo "## Activating SSL and mod_rewrite for Apache ##"
 echo "###############################################"
@@ -152,22 +143,6 @@ cd ..
 sudo chmod -R 777 code
 cd code/onlineshop/src
 sudo mysql -uonlineshop -pgeheim < onlineshop.sql
-
-echo "## permanently set Screen Resolution to 1920x1080 60 to optimize YouTube streaming ##"
-# Get parameters for xrandr
-# cvt 1920 1080 60
-# Get name of connected Screen.
-# xrandr
-
-XRANDR=/etc/X11/Xsession.d/45custom_xrandr-settings
-sudo bash -c "echo 'xrandr --newmode 1920x1080_60.00  173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync' >> $XRANDR"
-sudo bash -c "echo 'xrandr --addmode Virtual1 1920x1080_60.00' >> $XRANDR"
-sudo bash -c "echo 'xrandr --output Virtual1 --mode 1920x1080_60.00' >> $XRANDR"
-sudo chmod +x /etc/X11/Xsession.d/45custom_xrandr-settings
-
-echo "## adding empty document for Nautilus to Vorlagen ##"
-cd $HOME/Vorlagen
-touch "Leeres Document"
 
 echo "## reboot your system and finish installation with the steps below ##"
 echo "## Start jetbrains-toolbox from commandline or per click from Anwendungen and install PHPStorm ##"
