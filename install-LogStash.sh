6576899f7f03f31d004d9e77c072bbaeb6655526
Martin Harrer <18611980+martinharrer@users.noreply.github.com>
2021-05-07 16:38:29 +0200

    Update install-LogStash.sh

--- install-LogStash.sh
+++ install-LogStash.sh
@@ -2,9 +2,6 @@ sudo apt install logstash
 sudo bash -c "echo 'sudo systemctl start logstash' >> /usr/local/bin/StartLogstash.sh"
 sudo bash -c "echo 'sudo systemctl stop logstash' >> /usr/local/bin/StopLogstash.sh"
 sudo chmod a+x /usr/local/bin/*Logstash.sh
-sudo bash -c "echo 'StartLogstash.sh' >> /usr/local/bin/StartELK.sh"
-sudo bash -c "echo 'StopLogstash.sh' >> /usr/local/bin/StopELK.sh"
-sudo chmod a+x /usr/local/bin/*ELK.sh
 
 sudo bash -c "echo 'input {
   beats {
