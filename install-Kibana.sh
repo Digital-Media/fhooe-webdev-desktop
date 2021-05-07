f83b6c549a9bdf937bbad2c3b4f30170f606e8b6
Martin Harrer <mhteaching2703@gmail.com>
2021-05-07 04:30:07 -0700

    added install steps

--- install-Kibana.sh
+++ install-Kibana.sh
@@ -1,1 +1,4 @@
-
+sudo apt install kibana
+sudo bash -c "echo 'sudo systemctl start kibana' >> /usr/local/bin/StartKibana.sh"
+sudo bash -c "echo 'sudo systemctl stop kibana' >> /usr/local/bin/StopKibana.sh"
+sudo chmod a+x /usr/local/bin/*Kibana.sh
