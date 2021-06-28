dc7dad12fcafb2eb68ca25ab0a769a8a6f641bf1
MH <18611980+martinharrer@users.noreply.github.com>
2021-06-28 10:40:52 +0200

    added comment

--- install-FileBeat.sh
+++ install-FileBeat.sh
@@ -2,4 +2,4 @@ sudo apt install filebeat
 sudo bash -c "echo 'sudo systemctl start filebeat' >> /usr/local/bin/StartFilebeat.sh"
 sudo bash -c "echo 'sudo systemctl stop filebeat' >> /usr/local/bin/StopFilebeat.sh"
 sudo chmod a+x /usr/local/bin/*Filebeat.sh
-
+#
